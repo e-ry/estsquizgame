5d673d96283ea49c8e2fd83ba3120b34b73dc6f8
e-ry <erya@netlight.com>
2021-10-21 10:10:29 +0200

    setup basic postgresql databse containing questions, options and users

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
+const express = require('express');
+const app = express();
+const db = require('./queries');
+//const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
 // This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
-  //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT :)' }); //Line 10
-}); //Line 11
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT :)' });
+});
+
+app.get('/', (req, res) => {
+  res.send({ a: 'apple' });
+});
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.get('/options/:id', db.getOptionsById);
+app.get('/questions/:id', db.getQuestionById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
